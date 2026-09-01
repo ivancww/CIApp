ebf61b502174313ad129b9ba87c8ff0952cef9a4
ivancww <wahchiu0903@gmail.com>
2026-09-01 15:18:49 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Service Worker for Insurance PWA v9.6.9
-const CACHE_VERSION = 'insurance-pwa-cache-v9.6.9';
+// Service Worker for Insurance PWA v9.7.0
+const CACHE_VERSION = 'insurance-pwa-cache-v9.7.0';
 const FILES_TO_CACHE = [
   './',
   './index.html',
