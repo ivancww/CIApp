4614c5f16ba268f0013c94a56fc02491f9e38283
ivancww <wahchiu0903@gmail.com>
2026-09-16 00:57:21 +0800

    Align iPad workspace with 5pay design scale

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Service Worker for Insurance PWA v9.10.0
-const CACHE_VERSION = 'insurance-pwa-cache-v9.10.0';
+// Service Worker for Insurance PWA v9.11.0
+const CACHE_VERSION = 'insurance-pwa-cache-v9.11.0';
 const FILES_TO_CACHE = [
   './',
   './index.html',
